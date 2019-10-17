49aeccf90f3c92fd072c8214da196200f6919916
Milton Chicas <chicasm@ferris.edu>
2019-10-16 22:22:35 -0400

    Created Node Class and LinkedLists Class with append method

--- LinkedLists/LinkedLists/Program.cs
+++ LinkedLists/LinkedLists/Program.cs
@@ -6,7 +6,41 @@ namespace LinkedLists
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+        }
+    }
+
+    public class Node
+    {
+        public Node next;
+        int data;
+        //basic constructor
+        public Node(int data)
+        {
+            this.data = data;
+        }
+
+    }
+
+    public class LinkedList
+    {
+        Node head;
+        public void append(int data)
+        {
+            // if no head, it will be created
+            if (head  == null)
+            {
+                head = new Node(data);
+                return;
+            }
+            Node current = head;
+            // keep going checking next until there is no node
+            while (current.next != null)
+            {
+                current = current.next;
+            }
+            // create new node at end of linked list
+            current.next = new Node(data);
         }
     }
 }

2b40f5459f783d9474099128520f0283edd92c37
Milton Chicas <chicasm@ferris.edu>
2019-10-16 22:30:04 -0400

    Added delete method, renamed previous methods

--- LinkedLists/LinkedLists/Program.cs
+++ LinkedLists/LinkedLists/Program.cs
@@ -13,7 +13,7 @@ namespace LinkedLists
     public class Node
     {
         public Node next;
-        int data;
+        public int data;
         //basic constructor
         public Node(int data)
         {
@@ -25,7 +25,7 @@ namespace LinkedLists
     public class LinkedList
     {
         Node head;
-        public void append(int data)
+        public void addNode(int data)
         {
             // if no head, it will be created
             if (head  == null)
@@ -42,5 +42,40 @@ namespace LinkedLists
             // create new node at end of linked list
             current.next = new Node(data);
         }
+
+        public void addBefore(int data)
+        {
+            Node newHead = new Node(data);
+            newHead.next = head;
+            head = newHead;
+        }
+
+        public void deletewithValue(int data)
+        {
+            if (head == null)
+            {
+                return;
+            }
+            if (head.data == data)
+            {
+                head = head.next;
+                return;
+            }
+
+            Node current = head;
+            while (current.next != null)
+            {
+                if (current.next.data == data)
+                {
+                    current.next = current.next.next;
+                    return;
+                }
+                current = current.next;
+            }
+
+            
+        }
+
+
     }
 }
